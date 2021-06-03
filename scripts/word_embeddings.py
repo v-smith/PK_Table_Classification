93baab20b818a3353d7d1629961419588dbd4777
RMHIVSM <rmhivsm@ucl.ac.uk>
2021-06-03 16:10:47 +0100

    Initial commit

---
+++ scripts/word_embeddings.py
@@ -0,0 +1,8 @@
+import torch
+import torch.nn as nn
+
+word_to_ix = {"hello": 0, "world": 1}
+embeds = nn.Embedding(2, 5)  # 2 words in vocab, 5 dimensional embeddings
+lookup_tensor = torch.tensor([word_to_ix["hello"]], dtype=torch.long)
+hello_embed = embeds(lookup_tensor)
+print(hello_embed)
